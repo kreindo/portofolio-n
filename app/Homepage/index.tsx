30290f49dda3e81d8503db828405c999412545bd
kreindo <ahmadsan4p@gmail.com>
2022-10-30 13:28:25 +0800

    portofolio init

---
+++ app/Homepage/index.tsx
@@ -0,0 +1,10 @@
+export const Homepage = () => {
+  return (
+    <div className="bg-red-500">
+      <h1 className="font-medium text-blue-500">test</h1>
+      <section className="bg-pink-500">
+        <p className="font-bold text-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iste consequatur voluptas soluta, nostrum cum cupiditate eligendi similique officiis! At necessitatibus sed excepturi totam corrupti laborum voluptas vel quisquam! Beatae excepturi maiores quasi consectetur in iusto iste officia aliquid non laboriosam! Autem, suscipit perspiciatis quaerat nostrum sapiente nam similique! Sapiente sit fugiat repudiandae vel at totam nesciunt, iusto explicabo ducimus dignissimos, possimus repellendus nam optio consequatur iure dicta fugit voluptas ipsa. Magni, saepe porro laboriosam earum soluta corporis ducimus necessitatibus, eius consequatur a aliquam eaque illo dolorem inventore ipsa voluptates voluptas quidem nisi libero pariatur praesentium quis! Ipsam, velit eligendi!</p>
+      </section>
+    </div>
+  );
+};
