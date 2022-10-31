d90e2bd81cfd8f791c003d131a0c275cb97ec399
kreindo <ahmadsan4p@gmail.com>
2022-10-31 09:28:17 +0800

    added daggerT

--- app/Homepage/index.tsx
+++ app/Homepage/index.tsx
@@ -1,4 +1,5 @@
 import { Charo } from '../../components/Charo';
+import { DaggerT } from '../../components/DaggerT';
 import { EngineZero } from '../../components/EngineZero';
 export const Homepage = () => {
   return (
@@ -7,23 +8,7 @@ export const Homepage = () => {
       <EngineZero />
       <Charo />
       <Charo />
-      <h1 className="font-bold">test</h1>
-      <section>
-        <p className="font-medium">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-          iste consequatur voluptas soluta, nostrum cum cupiditate eligendi
-          similique officiis! At necessitatibus sed excepturi totam corrupti
-          laborum voluptas vel quisquam! Beatae excepturi maiores quasi
-          consectetur in iusto iste officia aliquid non laboriosam! Autem,
-          suscipit perspiciatis quaerat nostrum sapiente nam similique! Sapiente
-          sit fugiat repudiandae vel at totam nesciunt, iusto explicabo ducimus
-          dignissimos, possimus repellendus nam optio consequatur iure dicta
-          fugit voluptas ipsa. Magni, saepe porro laboriosam earum soluta
-          corporis ducimus necessitatibus, eius consequatur a aliquam eaque illo
-          dolorem inventore ipsa voluptates voluptas quidem nisi libero pariatur
-          praesentium quis! Ipsam, velit eligendi!
-        </p>
-      </section>
+      <DaggerT />
     </div>
   );
 };
---
+++ components/DaggerT/index.tsx
@@ -0,0 +1,23 @@
+export const DaggerT = () => {
+  return (
+    <div>
+      <h1 className="font-bold">test</h1>
+      <section>
+        <p className="font-medium">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          iste consequatur voluptas soluta, nostrum cum cupiditate eligendi
+          similique officiis! At necessitatibus sed excepturi totam corrupti
+          laborum voluptas vel quisquam! Beatae excepturi maiores quasi
+          consectetur in iusto iste officia aliquid non laboriosam! Autem,
+          suscipit perspiciatis quaerat nostrum sapiente nam similique! Sapiente
+          sit fugiat repudiandae vel at totam nesciunt, iusto explicabo ducimus
+          dignissimos, possimus repellendus nam optio consequatur iure dicta
+          fugit voluptas ipsa. Magni, saepe porro laboriosam earum soluta
+          corporis ducimus necessitatibus, eius consequatur a aliquam eaque illo
+          dolorem inventore ipsa voluptates voluptas quidem nisi libero pariatur
+          praesentium quis! Ipsam, velit eligendi!
+        </p>
+      </section>
+    </div>
+  );
+};
