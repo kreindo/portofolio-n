015c6b480bdf98eb9fd58d6a2ee65a311b61b43a
kreindo <ahmadsan4p@gmail.com>
2022-11-03 04:18:44 +0800

    dynamic menu

--- components/HeadMan/index.tsx
+++ components/HeadMan/index.tsx
@@ -1,13 +1,12 @@
 import { Fancer } from '../Fancer';
 
 export const HeadMan = () => {
+  const menus = ['Home', 'Content', 'Blog', 'Books', 'User']
   return (
     <div className="flex justify-between items-center flex-row">
-      <Fancer icon="Home" />
-      <Fancer icon="Content" />
-      <Fancer icon="Blog" />
-      <Fancer icon="Books" />
-      <Fancer icon="User" />
+      {menus.map((menu, index)=>{
+        return (<Fancer key={index} icon={menu}/>)
+      })}
     </div>
   );
 };
